2eda9ab8a372f417dae757ccc11bfe1a8ee270b8
Michael <mikek222@gmail.com>
2025-07-18 13:56:18 -0400

    moved files to their own folders, travel site done

---
+++ src/Stats.js
@@ -0,0 +1,22 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list!</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      {percentage === 100
+        ? "You got everything ready to go!"
+        : `👜 You have ${numItems} items on your list and you already packed ${" "}
+        ${numPacked} (${percentage}%)`}
+      <em></em>
+    </footer>
+  );
+}
